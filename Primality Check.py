144cfa89bf926bc80ad2d02269d2f31c9ca12f08
Dylan Leslie <dylan_leslie05@yahoo.com.au>
2024-10-05 12:47:10 +1000

    made it actually work by reusing factoring code. need to make it better, potentially use a sieve to generate primes below the prime checking then using those primes to check factors there to make it faster. or store the primes generated this way in a seperate spot to keep updating them to check it that way.

--- Primality Check.py
+++ Primality Check.py
@@ -1,30 +1,52 @@
 class PrimeChecker:
     def __init__(self):
         self.seen = set()
-        self.primeset = {2, 3}
+        self.primeset = set()
         self.primeDict = dict()
+
+    @staticmethod
+    def factors(n):
+        """
+        Gets the factors of n
+        :param n: number that you want the factors of
+        :type n: int
+        :return: a sorted list of factors
+        :rtype: set
+        """
+
+        factor_list = set()
+        for z in range(1, int(n ** 0.5) + 1):
+            if n % z == 0:
+                factor_list.add(z)
+                factor_list.add(n // z)
+        return sorted(factor_list)
+
     def PrimeCheck(self, i):
+
         if i in self.seen:
             return print(self.primeDict[i])
-        if i <= 3:
+        if i < 2:
             assert ValueError
         if type(i) is float:
             i = int(i)
-#        if type(i) is not int:
-#            assert TypeError
+        if type(i) is not int:
+            assert TypeError
         self.seen.add(i)
-        for n in range(2, int(i ** 0.5) + 1):
-#            for x in self.primeset.copy():
-#                if x % n != 0: # Doesnt work need fix
-#                    self.primeset.add(n)
-#                    self.primeDict[n] = 'Yes'
-#                else:
-#                    self.primeDict[n] = 'No'
-#            if i % n != 0: also doesnt work ffs
-#                self.primeset.add(i)
-#                self.primeDict[i] = 'Yes'
-#            else:
-#                self.primeDict[i] = 'No'
-#        return print(self.primeset, "{}, {} is prime".format(self.primeDict[i], i))
 
-PrimeChecker().PrimeCheck(548)
+        for p in range(1, i+1):
+            factor_list = self.factors(p)
+            if factor_list == [1, p]:
+                self.primeset.add(p)
+                self.primeDict[p] = 'Yes'
+            else:
+                self.primeDict[p] = 'No'
+
+        if self.primeDict[i] == 'Yes':
+            return_text = '{} {} is prime'.format(self.primeDict[i], i)
+        else:
+            return_text = '{} {} is not prime'.format(self.primeDict[i], i)
+
+        return print(self.primeDict, '\n', sorted(self.primeset), '\n', return_text)
+
+
+PrimeChecker().PrimeCheck(100007)
